2a7db45483b1a840e7d31357e60c710b95d4d66e
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-10-02 13:34:01 -0500

    version 1

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express =  require('express');
 const path = require('path');
 
 const app=express();
-
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname+'./dist/sistema-ventas-app'));
 app.get('/*',function (req,res) {
     res.sendFile(path.join(__dirname+'./dist/sistema-ventas-app/index.html'));
 })
-app.listen(8080);
+app.listen(app.get('port'));
