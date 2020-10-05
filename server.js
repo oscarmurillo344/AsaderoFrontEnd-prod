78de3d6f909401697a6a6955d465994d55cef5f3
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-10-05 18:29:15 -0500

    version 1

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app=express();
 
 app.use(express.static(__dirname+'dist/sistemaventasapp'));
 app.get('/*',function (req,res) {
-    res.sendFile(__dirname+'/../../dist/sistemaventasapp/index.html');
+    res.sendFile(path.join(__dirname+'/../../dist/sistemaventasapp/index.html'));
 })
 app.listen(process.env.PORT || 8080);
